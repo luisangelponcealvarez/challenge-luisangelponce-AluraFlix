72f2b847d115991faa7aa0473f4bd715f9b97137
LuisAngelPonceAlvarez <poncealvarezluisangel4@gmail.com>
2023-04-14 21:01:19 -0600

    añado mensaje

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Header from "./componentes/Header/Header";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DefauldPages from "./componentes/DefauldPages/DefauldPages";
+import Error404 from "./componentes/404/404";
 
 function App() {
   return (
     <div className="App">
+      <h1>Sitio en mantenimiento</h1>
       <Router>
-        <Header />
         <Routes>
-          <Route path="/" element={DefauldPages}/>
-
+          <Route path="/" element={DefauldPages} />
+          <Route path="*" element={<Error404 />} />
         </Routes>
       </Router>
     </div>
---
+++ src/componentes/404/404.jsx
@@ -0,0 +1,13 @@
+import "./404.css";
+import image from "../../../public/img/404.gif"
+
+function Error404() {
+  return (
+    <main className="container flex flex--center flex--column">
+      <img className="dog-image" src={image} alt="Doguito" />
+      <p className="notfound-text">Esta página no existe</p>
+    </main>
+  );
+}
+
+export default Error404;

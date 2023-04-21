a38e6aa97bee3a054eac895fc8f5dd7fcaf61ded
LuisAngelPonceAlvarez <poncealvarezluisangel4@gmail.com>
2023-04-20 21:11:50 -0600

    añado videos

---
+++ src/componentes/Videos/index.jsx
@@ -0,0 +1,13 @@
+import ReactPlayer from "react-player";
+function Videos(props) {
+  const { title, video } = props.lista;
+
+  return (
+    <>
+      <h2>{title}</h2>
+      <ReactPlayer url={video} controls />
+    </>
+  );
+}
+
+export default Videos;

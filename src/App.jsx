f0923eff2d74dd52a86c6eb2e65674ba88031ded
LuisAngelPonceAlvarez <poncealvarezluisangel4@gmail.com>
2023-04-21 21:39:52 -0600

    añado mensaje de 404 de enmantenimiento

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import DefauldPages from "./componentes/DefauldPages/DefauldPages";
 import Error404 from "./componentes/404/404";
+/* import DefauldPages from "./componentes/DefauldPages/DefauldPages";
 import NuevoVideo from "./componentes/NuevoVideo";
-import NuevaCategoria from "./componentes/NuevaCategoria";
+import NuevaCategoria from "./componentes/NuevaCategoria"; */
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<DefauldPages />} />
+          {/* <Route path="/" element={<DefauldPages />} />
           <Route path="/NuevoVideo" element={<NuevoVideo />} />
-          <Route path="/NuevaCategoria" element={<NuevaCategoria />} />
+          <Route path="/NuevaCategoria" element={<NuevaCategoria />} /> */}
           <Route path="*" element={<Error404 />} />
         </Routes>
       </Router>
--- src/componentes/404/404.jsx
+++ src/componentes/404/404.jsx
@@ -5,7 +5,7 @@ function Error404() {
   return (
     <main className="container flex flex--center flex--column">
       <img className="dog-image" src={image} alt="Doguito" />
-      <p className="notfound-text">Esta página no existe</p>
+      <p className="notfound-text">Esta página no existe estamos en mantenimiento</p>
     </main>
   );
 }
